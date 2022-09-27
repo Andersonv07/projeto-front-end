88067503c86817f967a8c1bae30302f478125941
Andersonv07 <anderson.rvf@gmail.com>
2022-09-27 17:31:56 -0300

    BLUNES.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// menu hamburguer
+const menu = document.querySelector('.container')
+function myFunction() {
+    menu.classList.toggle("change");
+   const nav = document.querySelector('.nav-header ') 
+   nav.classList.toggle("active")
+   
+  }
+ menu.addEventListener("click",myFunction)
+
+// menus flutuante
+
+ let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav-header").style.top = "0";
+  } else {
+    document.querySelector(".nav-header").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+// carregar mais
+
